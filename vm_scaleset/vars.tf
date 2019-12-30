49fdaa0b664540e156e7c74e6d0cf13b25546515
dnayy00@safeway.com <45861644+dksha17@users.noreply.github.com>
2019-12-30 13:57:34 +0530

    add

---
+++ vm_scaleset/vars.tf
@@ -0,0 +1,8 @@
+variable "resource_group_name" {}
+#variable "vnet" {}
+variable "subnet" {}
+variable "location" {}
+variable "backend_pool_name" {}
+variable "lb_name" {}
+variable "scaleset_name" {}
+variable "autoscaling_name" {}
